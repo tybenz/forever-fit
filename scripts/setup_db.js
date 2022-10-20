e4e25ff57920447263e803c7a66af941ea0dc5ad
Tyler Benziger <tabenziger@gmail.com>
2022-10-19 23:04:21 -0700

    Trying to understand where DB file is

--- scripts/setup_db.js
+++ scripts/setup_db.js
@@ -4,27 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const dbPath = path.join(__dirname, '..', 'db', 'db.json');
 
-// if (!fs.existsSync(dbPath)) {
-fs.writeFileSync(dbPath, JSON.stringify({ users: [
-    {
-      "phoneNumber": "2095813944",
-      "days": [
-      ],
-      "start": false,
-      "startTimezone": false,
-      "currentStreak": 0,
-      "maxStreak": 0,
-      "id": 1
-    },
-    {
-      "phoneNumber": "2096142267",
-      "days": [
-      ],
-      "start": false,
-      "startTimezone": false,
-      "currentStreak": 0,
-      "maxStreak": 0,
-      "id": 2
-    }
-] }));
-// }
+fs.writeFileSync(dbPath, JSON.stringify({users: []}));
+
+console.log(fs.readFileSync(dbPath, 'utf8'));
